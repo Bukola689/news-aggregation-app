676e323ab1766854b492229c652856d875b031cd
Bukola689 <jbukola95@gmail.com>
2025-08-05 14:10:52 +0100

    Auth Controller that conaiat Register, login user and logout using sanctum Api

---
+++ app/Http/Controllers/Api/V1/Auth/AuthenticationController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+     use ApiResponseTrait;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function register(UserRequest $request,UserService $userService): JsonResponse
+    {
+        $userDto = UserDto::fromApiFormRequest($request);
+        $user = $this->userService->createUser($userDto);
+        return $this->sendSuccess(['user'=> $user,], 'Register Successfull');
+    }
+
+   public function login(LoginRequest $request): JsonResponse   
+   {
+      $credentials = $request->validated();
+
+        if (empty($credentials['email']) || empty($credentials['password'])) {
+            return $this->sendError('Email and password are required', null, 422);
+        }
+
+        // Attempt to log the user in
+
+       if (!Auth::attempt($credentials)) {
+            return $this->sendError('Invalid credentials', null, 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return $this->sendSuccess([
+            'user' => $user,
+            'token' => $token
+        ], 'Login successful');
+   }
+
+        public function logout(Request $request): jsonResponse
+    {
+        if (!$request->user()) {
+            return $this->sendError('Unauthenticated', null, 401);
+        }
+        
+        $user = $request->user();
+
+        if ($user) {
+            $user->tokens()->delete();
+        }
+
+        return $this->sendSuccess([
+            'user' => $request->user(),
+        ], "Authentcated User Retrieved Successfully");
+    }
+
+    public function user(Request $request): jsonResponse
+    {
+        return $this->sendSuccess([
+            'user' => $request->user(),
+        ], "Authentcated User Retrieved Successfully");
+    }
+}
