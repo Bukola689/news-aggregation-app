6ffb76d507650dc6b246df7c522e16773bf7a17d
Bukola689 <jbukola95@gmail.com>
2025-08-05 13:58:51 +0100

    create user-Service for User

---
+++ app/Services/UserService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\Dtos\UserDto;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use App\Exceptions\ModeNotFoundException;
+use App\Exceptions\PinHasAlreadyBeenSetException;
+use App\Exceptions\PinNotSetException;
+use Illuminate\Database\Eloquent\Builder;
+use App\Interfaces\UserServiceInterface;
+use App\Exceptions\InvalidPinLengthException;
+use Illuminate\Support\Facades\Hash;
+
+class UserService implements UserServiceInterface
+{
+    public function createUser(UserDto $userDto): Model
+    {
+      return User::query()->create([
+            'name' => $userDto->getName(),
+            'email' => $userDto->getEmail(),
+            'password' => Hash::make($userDto->getPassword()),
+        ]);
+    }
+
+    public function modelQuery(): Builder
+    {
+        return User::query();
+    }
+
+      public function getUserById(int $userId): Model
+    {
+        $user = User::query()->find($userId);
+
+        if (!$user) {
+            throw new ModeNotFoundException('User Not Found');
+        }
+
+        return $user;
+    }
+}
